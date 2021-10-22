38a851075f127ac03a05b679c6f7ac1b05e9dd42
limpido <jiichuan0325@gmail.com>
2021-10-22 22:22:31 +0800

    add auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {AuthService} from "../services/auth.service";
+import {User} from "../models/user";
+import {first} from "rxjs/operators";
+import {UserService} from "../services/user.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+  ) {
+  }
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    const uid = route.url[0].path;
+    const user: User = this.authService.user ?? await this.authService.user$.pipe(first()).toPromise();
+    const author: User = await this.userService.getUserByUid(uid).pipe(first()).toPromise();
+    if (user && user.uid == author.uid) {
+      return true;
+    } else return this.router.parseUrl(`/`);
+  }
+
+}
