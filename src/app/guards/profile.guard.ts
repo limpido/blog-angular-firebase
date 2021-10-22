c5a728978f8c50f28b03c3c055ffc5a993b1cc9b
limpido <jiichuan0325@gmail.com>
2021-10-22 22:22:44 +0800

    add profile guard

---
+++ src/app/guards/profile.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {AuthService} from "../services/auth.service";
+import {User} from "../models/user";
+import {first} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    const user: User = this.authService.user ?? await this.authService.user$.pipe(first()).toPromise();
+    return !!user ? true : this.router.parseUrl(`/`);
+  }
+
+}
