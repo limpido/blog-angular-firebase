acb094918f8fa8b5872eb973623f67a4a85fdf21
limpido <jiichuan0325@gmail.com>
2022-01-08 21:15:22 +0800

    update angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNceHHaOSBX_cP4f_-rSJf4RGqm0fVfAs",
+    authDomain: "blog-angular-firebase-feb70.firebaseapp.com",
+    // databaseURL: '<your-database-URL>',
+    projectId: "blog-angular-firebase-feb70",
+    storageBucket: "blog-angular-firebase-feb70.appspot.com",
+    messagingSenderId: "199536931828",
+    appId: "1:199536931828:web:f1ffaa179f93df80523fa1",
+    measurementId: "G-WS1VXTFYG5"
+  }
 };
